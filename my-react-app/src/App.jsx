cec3e4f24de7434c88b0b1696df95918cd431d8c
nbil-s <bilgis.nzembi@strathmore.educd>
2025-06-12 23:18:40 +0300

    Dashboard

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,6 +4,8 @@ import NavBar from './components/NavBar.jsx';
 import HomePage from './pages/HomePage.jsx';
 import Login from './pages/Login.jsx';
 import Signup from './pages/Signup.jsx';
+import Dashboard from './pages/Dashboard.jsx'; 
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/homepage" element={<HomePage />} />
         <Route path="/signup" element={<Signup/>} />
+       <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+
       </Routes>
     </>
   );
