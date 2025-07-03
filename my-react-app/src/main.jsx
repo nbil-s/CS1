d5e2dbb5584ac82ae8694d02fd6b2870422d8a9c
nbil-s <bilgis.nzembi@strathmore.educd>
2025-07-03 23:44:21 +0300

    updates

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -4,14 +4,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthProvider } from './contexts/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
 )
